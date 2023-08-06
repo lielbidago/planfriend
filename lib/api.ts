39283f9e9533c582338f8dffcac4599b6a850fc8
lielbidago <liloplev@gmail.com>
2023-08-06 19:32:15 +0300

    db issue

--- lib/api.ts
+++ lib/api.ts
@@ -8,7 +8,9 @@ interface fetcherProps{
 }
 
 const fetcher = async ({ url, method, body, json = true }:fetcherProps) => {
-    const res = await fetch(url, {
+    
+  console.log(`url: ${url}`)
+  const res = await fetch(url, {
       method,
       body: body && JSON.stringify(body),
       headers: {
